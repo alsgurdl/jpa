3f62cde15b9431eeed8346b1893b4647e6ed9528
alsgurdl <fdf98952@gmail.com>
2024-05-16 15:57:50 +0900

    5-9

--- src/main/java/com/study/jpa/chap05_practice/api/PostApiController.java
+++ src/main/java/com/study/jpa/chap05_practice/api/PostApiController.java
@@ -1,13 +1,26 @@
 package com.study.jpa.chap05_practice.api;
 
-import com.study.jpa.chap05_practice.dto.PageDTO;
-import com.study.jpa.chap05_practice.dto.PostListResponseDTO;
+import com.study.jpa.chap05_practice.dto.*;
 import com.study.jpa.chap05_practice.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@Tag(name = "post API", description = "게시물 등록및 삭제 api입니다")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -20,7 +33,7 @@ public class PostApiController {
     /*
         게시물 목록 조회: /posts            - GET, param: (page, size)
         게시물 개별 조회: /posts/{id}       - GET
-        게시물 등록:     /posts            - POST
+        게시물 등록:     /posts            - POST, payload: (writer, title, content, hashTags)
         게시물 수정:     /posts/{id}       - PATCH
         게시물 삭제:     /posts/{id}       - DELETE
      */
@@ -33,28 +46,104 @@ public class PostApiController {
 
         return ResponseEntity.ok().body(dto);
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> detail(@PathVariable Long id){
-        log.info("api/v1/pots/{}", id);
-        return null;
+    public ResponseEntity<?> detail(@PathVariable Long id) {
+        log.info("/api/v1/posts/{}: GET", id);
+
+        try {
+            PostDetailResponseDTO dto = postService.getDetail(id);
+            return ResponseEntity.ok().body(dto);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 
+@Operation(summary = "게시물 등록", description = "게시물 작성 및 등록을 담당하는 메서드")
+   @Parameters({
+           @Parameter(name = "writer", description = "게시물의 작성자 이름을 쓰세요!", example = "김뽀삐", required = true),
+           @Parameter(name = "title", description = "게시물의 제목을 쓰세요!", example = "제목제목", required = true),
+           @Parameter(name = "content", description = "게시물의 내용을 쓰세요!", example = "내용내용"),
+           @Parameter(name = "hashTags", description = "게시물의 해시태그를 작성하세요!", example = "['하하', '호호']")
+   })
+    @PostMapping
+    public ResponseEntity<?> create(
+            @Validated @RequestBody PostCreateDTO dto,
+            BindingResult result // 검증 에러 정보를 가진 객체
+    ) {
+        log.info("/api/v1/posts POST!! - payload: {}", dto);
+
+        if (dto == null) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("등록 게시물 정보를 전달해 주세요!");
+        }
+
+        ResponseEntity<List<FieldError>> fieldErrors = getValidatedResult(result);
+        if (fieldErrors != null) return fieldErrors;
+
+        // 위에 존재하는 if문을 모두 건너뜀 -> dto가 null도 아니고, 입력값 검증도 모두 통과함 -> service에게 명령.
+        try {
+            PostDetailResponseDTO responseDTO = postService.insert(dto);
+            return ResponseEntity
+                    .ok()
+                    .body(responseDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity
+                    .internalServerError()
+                    .body("서버 에러 원인: " + e.getMessage());
+        }
+
+
+    }
+
+    private static ResponseEntity<List<FieldError>> getValidatedResult(BindingResult result) {
+        if (result.hasErrors()) { // 입력값 검증 단계에서 문제가 있었다면 true
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            fieldErrors.forEach(err -> {
+                log.warn("invalid client data - {}", err.toString());
+            });
+            return ResponseEntity
+                    .badRequest()
+                    .body(fieldErrors);
+        }
+        return null;
+    }
+
+    @Operation(summary = "게시글 수정",description = "게시물 수정을 담당하는 메서드")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "수정완료",
+                    content = @Content(schema = @Schema(implementation = PostDetailResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "입력값 검증 실패"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND")
+    })
+    @RequestMapping(method = {RequestMethod.PUT,RequestMethod.PATCH})
+    public ResponseEntity<?> update(
+        @Validated @RequestBody PostModifyDTO dto,
+        BindingResult result,
+        HttpServletRequest request
+){
+ log.info("pots{} - payload {}", request.getMethod(),dto);
+
+        ResponseEntity<List<FieldError>> fieldErrors = getValidatedResult(result);
+        if (fieldErrors != null)return fieldErrors;
+
+        PostDetailResponseDTO responseDTO = postService.modify(dto);
+
+        return ResponseEntity.ok().body(responseDTO);
+
+    }
+
+    //게시물 삭제
+    //테스트 진행 시 해시태그가 없는 글을 삭제 해보세요
+    @DeleteMapping("/{id}")
+    private ResponseEntity<?> delete(@PathVariable Long id){
+       log.info("posts/{}",id);
+        postService.Delete(id);
+
+        return ResponseEntity.ok("delSuccess!");
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/main/java/com/study/jpa/chap05_practice/entity/Post.java
+++ src/main/java/com/study/jpa/chap05_practice/entity/Post.java
@@ -39,8 +39,22 @@ public class Post {
     @UpdateTimestamp
     private LocalDateTime updateDate; // 수정 시간
 
-    @OneToMany(mappedBy = "post")
+    @OneToMany(mappedBy = "post",orphanRemoval = true)
+    @Builder.Default // 특정 필드를 객체를 새엇할 때 특정 필드를 저장 지정한 값을 초기화 하는 것을 강제
     List<HashTag> hashTags = new ArrayList<>();
+
+    //앵방향 관계에서 리스트 쪽에 데이터를 추가하는 편의 메서드
+    //insert > select를 진행하는 것도 가능하지만 entitymanger를 이용
+    //insert와 동시에 실시간으로 리스트에 동기화
+    public void  addHashTag(HashTag hashTag){
+        this.hashTags.add(hashTag); //매개값으로 전달받은 HashTag 객체를 리스트에 추가
+//전달된 HashTag 객체가 가지고 있는 post가
+        //이 메서드를 부르는 post 객체와 주소값이 서로 다르다면 데이터 불일치가 발새하기 때문에
+        //HashTag의 post도 이객체 변경
+        if (this != hashTag.getPost()){
+            hashTag.setPost(this);
+        }
+    }
     
 }
 
